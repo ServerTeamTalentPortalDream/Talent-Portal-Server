7862faa7b0d18ed8150aa1870d19903052a84dc5
Josue Rodriguez <jrod_1729@hotmail.com>
2018-10-01 15:04:40 -0400

    Fixed bug

--- SkillsCertsService/SkillsAndCert/src/main/java/com/revature/controllers/CertificationsController.java
+++ SkillsCertsService/SkillsAndCert/src/main/java/com/revature/controllers/CertificationsController.java
@@ -14,7 +14,7 @@ import com.revature.services.CertifiactionsService;
 
 @CrossOrigin
 @RestController
-@RequestMapping(name = "certifications")
+@RequestMapping("certifications")
 public class CertificationsController {
 
 	@Autowired
