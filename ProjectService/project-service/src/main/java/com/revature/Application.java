c8103bebf91189ea2e8330764a3f6ba256930e6d
Aulden <aulden.harris@gmail.com>
2018-10-02 11:04:00 -0400

    Added Swagger Docs to Project Service

--- ProjectService/project-service/src/main/java/com/revature/Application.java
+++ ProjectService/project-service/src/main/java/com/revature/Application.java
@@ -2,9 +2,12 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 // import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableSwagger2
 // @EnableDiscoveryClient
 public class Application {
 	public static void main(String[] args) throws Exception {
