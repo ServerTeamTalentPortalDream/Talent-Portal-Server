feefe63543f30b97009371759945bf396479da66
Aulden <aulden.harris@gmail.com>
2018-10-02 10:48:17 -0400

    Added Swagger Docs to Eureka Service

--- Microservices/discovery-service/src/main/java/com/revature/Application.java
+++ Microservices/discovery-service/src/main/java/com/revature/Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableEurekaServer
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
