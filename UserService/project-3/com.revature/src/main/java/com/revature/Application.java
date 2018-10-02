1c757429e868eb88db4c04b4cc836559a7478297
Aulden <aulden.harris@gmail.com>
2018-10-02 09:41:35 -0400

    Added Swagger Docs to User Service

--- UserService/project-3/com.revature/src/main/java/com/revature/Application.java
+++ UserService/project-3/com.revature/src/main/java/com/revature/Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
 	public static void main(String[] args) {
