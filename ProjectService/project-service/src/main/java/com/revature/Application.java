5c9afae744c14fb21445a0306a966e17c605e1a1
Kevin Pham <kpthanh01@gmail.com>
2018-10-05 12:05:23 -0400

    added microservices settings

--- ProjectService/project-service/src/main/java/com/revature/Application.java
+++ ProjectService/project-service/src/main/java/com/revature/Application.java
@@ -2,13 +2,14 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 @EnableSwagger2
-// @EnableDiscoveryClient
+//@EnableEurekaClient
 public class Application {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
