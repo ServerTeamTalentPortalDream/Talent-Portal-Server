d2fbab5119cc6632b172ed63e8e90d5323bfa4e3
Spencer Rendon <sarendon@bellsouth.net>
2018-10-09 11:46:28 -0400

    Hystrix Project Services

--- ProjectService/project-service/src/main/java/com/revature/Application.java
+++ ProjectService/project-service/src/main/java/com/revature/Application.java
@@ -4,13 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @EnableSwagger2
-//@EnableCircuitBreaker
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+
 //@EnableEurekaClient
 public class Application {
 	public static void main(String[] args) throws Exception {
