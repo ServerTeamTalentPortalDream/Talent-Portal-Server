45b8b9c1bf85e4511549bc6452bf6dec10f8fff7
Daniel Clark <42151146+DClarkyd@users.noreply.github.com>
2018-10-09 10:37:57 -0400

    Revert "Micro services"

--- ProjectService/project-service/src/main/java/com/revature/repos/ProjectRepository.java
+++ ProjectService/project-service/src/main/java/com/revature/repos/ProjectRepository.java
@@ -1,7 +1,6 @@
 package com.revature.repos;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +9,5 @@ import com.revature.model.Project;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
-	Optional<Project> findByProjectId(int projectId);
 	List<Project> findAllByOrderByStartDate();
 }
