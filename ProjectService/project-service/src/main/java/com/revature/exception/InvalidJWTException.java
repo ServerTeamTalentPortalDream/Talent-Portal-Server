7c136a765b5e0163e4d979e11036697f317b8242
Aulden <aulden.harris@gmail.com>
2018-10-11 13:25:06 -0400

    The Purgeoning

--- ProjectService/project-service/src/main/java/com/revature/exception/InvalidJWTException.java
+++
@@ -1,9 +0,0 @@
-package com.revature.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value=HttpStatus.UNAUTHORIZED)
-public class InvalidJWTException extends RuntimeException{
-	// invalid jwt returns unauthorized
-}
