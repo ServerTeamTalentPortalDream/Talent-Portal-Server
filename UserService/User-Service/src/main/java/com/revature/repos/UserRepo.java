62823062fa8cbad118d8cedf26cdfc80e6d1a339
Kevin Pham <kpthanh01@gmail.com>
2018-10-12 11:47:55 -0400

    added unauthorization

--- UserService/User-Service/src/main/java/com/revature/repos/UserRepo.java
+++ UserService/User-Service/src/main/java/com/revature/repos/UserRepo.java
@@ -1,18 +1,18 @@
 package com.revature.repos;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.revature.models.User;
-import com.revature.models.UserSkills;
 
 @Repository
 public interface UserRepo extends JpaRepository <User, Integer>{
 
 //	User findByUsername(String username);
-	User findByUserId(int userId);
+	Optional<User> findByUserId(int userId);
 	User findByUserIdAndPass(int userId, String pass);
 	User findByUserIdAndEmail(int userId, String email);
 	User findByAssociateId(int associateId);
